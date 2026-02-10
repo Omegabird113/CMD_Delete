ec6f281967459429a816f2945707f47560f4ae28
omegabird113 <omegabird113@outlook.com>
2026-02-10 10:04:45 -0500

    Added More Comments

--- src/main/java/mc/omegabird/cmd_delete/client/cmdDeleteClient.java
+++ src/main/java/mc/omegabird/cmd_delete/client/cmdDeleteClient.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ClientModInitializer;
 import org.lwjgl.glfw.GLFW;
 
 public class cmdDeleteClient implements ClientModInitializer {
+    // Ints w/ all the Keys
     static int leftControl = GLFW.GLFW_KEY_LEFT_CONTROL;
     static int leftAltOption = GLFW.GLFW_KEY_LEFT_ALT;
     static int leftSuperCommand = GLFW.GLFW_KEY_LEFT_SUPER;
@@ -11,11 +12,13 @@ public class cmdDeleteClient implements ClientModInitializer {
     static int rightAltOption = GLFW.GLFW_KEY_RIGHT_ALT;
     static int rightSuperCommand = GLFW.GLFW_KEY_RIGHT_SUPER;
 
+    // Method to check if user's on macOS
     public static boolean ifUserOnMac() {
-        String os = System.getProperty("os.name").toLowerCase();
-        return os.contains("mac");
+        String os = System.getProperty("os.name").toLowerCase(); // gets os name
+        return os.contains("mac"); // returns if name has 'mac' in it
     }
 
+    // Other word = control, macOS = option. other line = unknown, macOS = command. Sets left/right appropriately
     public static int WORD_MODIFIER_KEY = ifUserOnMac() ? leftAltOption : leftControl;
     public static int LINE_MODIFIER_KEY = ifUserOnMac() ? leftSuperCommand : GLFW.GLFW_KEY_UNKNOWN;
     public static int RIGHT_WORD_MODIFIER_KEY = ifUserOnMac() ? rightAltOption : rightControl;
