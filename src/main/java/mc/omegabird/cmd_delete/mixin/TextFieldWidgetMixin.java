d98f0304bdf8a16b4bc11583d797dc44db9e381b
omegabird113 <omegabird113@outlook.com>
2026-02-28 08:39:56 -0500

    Migrate to Mojang Mappings

--- src/main/java/mc/omegabird/cmd_delete/mixin/TextFieldWidgetMixin.java
+++ src/main/java/mc/omegabird/cmd_delete/mixin/TextFieldWidgetMixin.java
@@ -1,14 +1,14 @@
 package mc.omegabird.cmd_delete.mixin;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import mc.omegabird.cmd_delete.client.cmdDeleteClient;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.widget.ClickableWidget;
-import net.minecraft.client.gui.widget.TextFieldWidget;
-import net.minecraft.client.input.KeyInput;
-import net.minecraft.client.util.InputUtil;
-import net.minecraft.text.Text;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.AbstractWidget;
+import net.minecraft.client.gui.components.EditBox;
+import net.minecraft.client.input.KeyEvent;
+import net.minecraft.network.chat.Component;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -16,37 +16,37 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(value = TextFieldWidget.class, priority = 2000)
-public abstract class TextFieldWidgetMixin extends ClickableWidget {
+@Mixin(value = EditBox.class, priority = 2000)
+public abstract class TextFieldWidgetMixin extends AbstractWidget {
     @Environment(EnvType.CLIENT)
-    public TextFieldWidgetMixin(int x, int y, int width, int height, Text message) {
+    public TextFieldWidgetMixin(int x, int y, int width, int height, Component message) {
         super(x, y, width, height, message);
     }
 
     @Shadow
-    protected abstract void erase(int offset, boolean words);
+    protected abstract void deleteText(int offset, boolean words);
     @Shadow
-    public abstract void eraseCharactersTo(int position);
+    public abstract void deleteCharsToPos(int position);
     @Shadow
-    public abstract void setCursor(int cursor, boolean select);
+    public abstract void moveCursorTo(int cursor, boolean select);
     @Shadow
-    public abstract String getText();
+    public abstract String getValue();
     @Shadow
-    public abstract int getWordSkipPosition(int wordOffset);
+    public abstract int getWordPosition(int wordOffset);
 
     static {
         cmdDeleteClient.LOGGER.info("Registering TextFieldWidgetMixin");
     }
     
     @Inject(method = "keyPressed", at = @At("HEAD"), cancellable = true)
-    private void cmd_delete$overrideDelete(KeyInput input, CallbackInfoReturnable<Boolean> cir) {
+    private void cmd_delete$overrideDelete(KeyEvent input, CallbackInfoReturnable<Boolean> cir) {
         int key = input.key();
-        var window = MinecraftClient.getInstance().getWindow();
-        boolean shift = input.hasShift(); // gets is shift down
+        var window = Minecraft.getInstance().getWindow();
+        boolean shift = input.hasShiftDown(); // gets is shift down
 
         // gets left/right word/line keys down
-        boolean word = InputUtil.isKeyPressed(window, cmdDeleteClient.WORD_MODIFIER_KEY) || InputUtil.isKeyPressed(window, cmdDeleteClient.RIGHT_WORD_MODIFIER_KEY);
-        boolean line = InputUtil.isKeyPressed(window, cmdDeleteClient.LINE_MODIFIER_KEY) || InputUtil.isKeyPressed(window, cmdDeleteClient.RIGHT_LINE_MODIFIER_KEY);
+        boolean word = InputConstants.isKeyDown(window, cmdDeleteClient.WORD_MODIFIER_KEY) || InputConstants.isKeyDown(window, cmdDeleteClient.RIGHT_WORD_MODIFIER_KEY);
+        boolean line = InputConstants.isKeyDown(window, cmdDeleteClient.LINE_MODIFIER_KEY) || InputConstants.isKeyDown(window, cmdDeleteClient.RIGHT_LINE_MODIFIER_KEY);
 
         // delete handling
         if (key == GLFW.GLFW_KEY_BACKSPACE || key == GLFW.GLFW_KEY_DELETE) {
@@ -65,11 +65,11 @@ public abstract class TextFieldWidgetMixin extends ClickableWidget {
 
             if (line) {
                 // Deletes begining/end based off direction. If backspace before, delete to 0, else delete after delete to end length.
-                this.eraseCharactersTo(direction < 0 ? 0 : this.getText().length());
+                this.deleteCharsToPos(direction < 0 ? 0 : this.getValue().length());
                 cmdDeleteClient.LOGGER.debug("Deletion handled as LINE. Direction = {}", direction);
             } else {
                 // This handles the word deletion
-                this.erase(direction, true);
+                this.deleteText(direction, true);
                 cmdDeleteClient.LOGGER.debug("Deletion handled as WORD. Direction = {}", direction);
             }
 
@@ -92,11 +92,11 @@ public abstract class TextFieldWidgetMixin extends ClickableWidget {
 
             if (line) {
                 // moves to begining/end based on direction (if -1 left, then move to first, else move to right based opn length) & passes shift
-                this.setCursor(direction < 0 ? 0 : this.getText().length(), shift);
+                this.moveCursorTo(direction < 0 ? 0 : this.getValue().length(), shift);
                 cmdDeleteClient.LOGGER.debug("Navigation handled as LINE. Direction = {}", direction);
             } else {
                 // moves to position based on word skip position passing direction and shift
-                this.setCursor(this.getWordSkipPosition(direction), shift);
+                this.moveCursorTo(this.getWordPosition(direction), shift);
                 cmdDeleteClient.LOGGER.debug("Navigation handled as WORD. Direction = {}", direction);
             }
 
