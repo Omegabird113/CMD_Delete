1c87eaa128fd3275a0ba52180babc38082306684
omegabird113 <omegabird113@outlook.com>
2026-02-28 07:05:04 -0500

    Update modid to cmd-delete

--- src/main/java/mc/omegabird/cmd_delete/client/cmdDeleteClient.java
+++ src/main/java/mc/omegabird/cmd_delete/client/cmdDeleteClient.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class cmdDeleteClient implements ClientModInitializer {
-    public static final String MOD_ID = "cmd_delete";
+    public static final String MOD_ID = "cmd-delete";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     // Ints w/ all the Keys
