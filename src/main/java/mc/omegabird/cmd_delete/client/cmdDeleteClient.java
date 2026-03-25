fb4012c14bec3b96587213daded63bf6879b5b1f
omegabird113 <omegabird113@outlook.com>
2026-03-25 19:48:48 -0400

    Code clarity & organization improvements

--- src/main/java/mc/omegabird/cmd_delete/client/cmdDeleteClient.java
+++ src/main/java/mc/omegabird/cmd_delete/client/cmdDeleteClient.java
@@ -6,8 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class cmdDeleteClient implements ClientModInitializer {
-    public static final String MOD_ID = "cmd-delete";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final String MODID = "cmd-delete";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
 
     private static final int leftControl = GLFW.GLFW_KEY_LEFT_CONTROL;
     private static final int leftAltOption = GLFW.GLFW_KEY_LEFT_ALT;
@@ -16,15 +16,15 @@ public class cmdDeleteClient implements ClientModInitializer {
     private static final int rightAltOption = GLFW.GLFW_KEY_RIGHT_ALT;
     private static final int rightSuperCommand = GLFW.GLFW_KEY_RIGHT_SUPER;
 
-    public static boolean ifUserOnMac() {
-        String os = System.getProperty("os.name").toLowerCase(); // gets os name
-        return os.contains("mac");
-    }
-
     public static final int WORD_MODIFIER_KEY = ifUserOnMac() ? leftAltOption : leftControl;
     public static final int LINE_MODIFIER_KEY = ifUserOnMac() ? leftSuperCommand : GLFW.GLFW_KEY_UNKNOWN;
     public static final int RIGHT_WORD_MODIFIER_KEY = ifUserOnMac() ? rightAltOption : rightControl;
     public static final int RIGHT_LINE_MODIFIER_KEY = ifUserOnMac() ? rightSuperCommand : GLFW.GLFW_KEY_UNKNOWN;
+
+    public static boolean ifUserOnMac() {
+        String os = System.getProperty("os.name").toLowerCase(); // gets os name
+        return os.contains("mac");
+    }
 
     @Override
     public void onInitializeClient() {
