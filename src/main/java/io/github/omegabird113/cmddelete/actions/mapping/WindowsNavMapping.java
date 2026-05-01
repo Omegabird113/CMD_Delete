cee215ad83f116230e29ce5b70f093747fc7d7fd
omegabird113 <omegabird113@outlook.com>
2026-05-01 19:00:37 -0400

    Improve formatting in WindowsNavMapping

--- src/main/java/io/github/omegabird113/cmddelete/actions/mapping/WindowsNavMapping.java
+++ src/main/java/io/github/omegabird113/cmddelete/actions/mapping/WindowsNavMapping.java
@@ -61,11 +61,11 @@ public class WindowsNavMapping implements INavMapping {
     @Override
     public ActionConstant[] getPossibleActions() {
         return new ActionConstant[] {
-                NAV_LINE_LEFT,  NAV_LINE_RIGHT,
-                SEL_LINE_LEFT,  SEL_LINE_RIGHT,
-                DEL_WORD_LEFT,  DEL_WORD_RIGHT,
-                NAV_WORD_LEFT,  NAV_WORD_RIGHT,
-                SEL_WORD_LEFT,  SEL_WORD_RIGHT,
+                NAV_LINE_LEFT, NAV_LINE_RIGHT,
+                SEL_LINE_LEFT, SEL_LINE_RIGHT,
+                DEL_WORD_LEFT, DEL_WORD_RIGHT,
+                NAV_WORD_LEFT, NAV_WORD_RIGHT,
+                SEL_WORD_LEFT, SEL_WORD_RIGHT,
                 NAV_TEXT_START, NAV_TEXT_END,
                 SEL_TEXT_START, SEL_TEXT_END,
                 SEL_TEXT_UP, SEL_TEXT_DOWN
