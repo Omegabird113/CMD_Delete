4edc8466df527ef51df430508114eb61a960dbe2
omegabird113 <omegabird113@outlook.com>
2026-06-05 11:45:05 -0400

    Don't hard-code the version twice

--- src/main/java/io/github/omegabird113/cmd_delete/CmdDeleteClient.java
+++ src/main/java/io/github/omegabird113/cmd_delete/CmdDeleteClient.java
@@ -3,12 +3,17 @@ package io.github.omegabird113.cmd_delete;
 import io.github.omegabird113.cmd_delete.commands.NavMappingsCommand;
 import io.github.omegabird113.cmd_delete.mappings.NavMappingsManager;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Optional;
+
 public class CmdDeleteClient implements ClientModInitializer {
     public static final String MODID = "cmd_delete";
-    public static final String VERSION = "1.0.0-rc1+mc26.1";
+    public static final Optional<ModContainer> CONTAINER = FabricLoader.getInstance().getModContainer(MODID);
+    public static final String VERSION =  CONTAINER.isEmpty() ? "" : CONTAINER.get().getMetadata().getVersion().getFriendlyString();
     public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
 
     @Override
