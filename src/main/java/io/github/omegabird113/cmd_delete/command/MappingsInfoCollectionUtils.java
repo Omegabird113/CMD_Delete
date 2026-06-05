e0dc73566403e18356beaebe54217a6cc376ba20
omegabird113 <omegabird113@outlook.com>
2026-06-05 13:31:02 -0400

    Add filename to CustomMappingsRegistry & fix custom mappings ids in MappingsInfoCollectionUtils

--- src/main/java/io/github/omegabird113/cmd_delete/command/MappingsInfoCollectionUtils.java
+++ src/main/java/io/github/omegabird113/cmd_delete/command/MappingsInfoCollectionUtils.java
@@ -22,7 +22,7 @@ public final class MappingsInfoCollectionUtils {
             case INavMappings n when n instanceof MacNavMappings || n instanceof WindowsLinuxNavMappings ->
                     "builtin:" + Arrays.toString(navMappings.getMappingsSupportedSystems()).replace("[", "").replace("]", "").replace(", ", "_").toLowerCase();
             case INavMappings n when n instanceof CustomNavMappings ->
-                    "custom";
+                    "custom:" + ((CustomNavMappings) navMappings).getRegistry().getFilename();
             default -> "unknown";
         };
 
