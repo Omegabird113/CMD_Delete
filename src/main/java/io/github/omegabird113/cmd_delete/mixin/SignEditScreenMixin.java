1a8a30880246b762f1638271cc545a9c86142243
omegabird113 <omegabird113@outlook.com>
2026-06-13 10:29:05 -0400

    1.21.5 port

--- src/main/java/io/github/omegabird113/cmd_delete/mixin/SignEditScreenMixin.java
+++ src/main/java/io/github/omegabird113/cmd_delete/mixin/SignEditScreenMixin.java
@@ -9,6 +9,7 @@ import net.minecraft.client.gui.font.TextFieldHelper;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.AbstractSignEditScreen;
 import net.minecraft.client.renderer.RenderPipelines;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.level.block.entity.SignBlockEntity;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Final;
@@ -136,7 +137,7 @@ public abstract class SignEditScreenMixin {
             int x2 = this.cmd_delete$getTextAtX(message, end);
             int y = workingLine * textLineHeight - yOffset;
 
-            guiGraphics.fill(RenderPipelines.GUI_TEXT_HIGHLIGHT, Math.min(x1, x2), y, Math.max(x1, x2), y + textLineHeight, -16776961);
+            guiGraphics.fill(RenderType.guiTextHighlight(), Math.min(x1, x2), y, Math.max(x1, x2), y + textLineHeight, -16776961);
         }
     }
 
