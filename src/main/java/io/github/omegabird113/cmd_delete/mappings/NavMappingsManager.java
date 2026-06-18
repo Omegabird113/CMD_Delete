12f79e57127a969c43086e05de940332ee956895
omegabird113 <omegabird113@outlook.com>
2026-06-18 15:35:51 -0400

    Log registry size

--- src/main/java/io/github/omegabird113/cmd_delete/mappings/NavMappingsManager.java
+++ src/main/java/io/github/omegabird113/cmd_delete/mappings/NavMappingsManager.java
@@ -22,7 +22,7 @@ public final class NavMappingsManager {
     }
 
     private static void logMappings() {
-        CmdDeleteClient.LOGGER.info("Mappings id \"{}\" loaded with supported systems \"{}\" and Coverage of {}%", activeMappingsManager.resolveNamespacedId(currentMappingsState), List.of(currentMappingsState.mappings().getMappingsSupportedSystems()), NavActionUtils.getCoverage(getCurrentMappings()) * 100);
+        CmdDeleteClient.LOGGER.info("Mappings id \"{}\" loaded with supported systems \"{}\" and Coverage of {}% with a registry size of {}", activeMappingsManager.resolveNamespacedId(currentMappingsState), List.of(currentMappingsState.mappings().getMappingsSupportedSystems()), NavActionUtils.getCoverage(getCurrentMappings()) * 100, currentMappingsState.mappings().getRegistry().getSize());
         CmdDeleteClient.LOGGER.info("The active mappings' info shows as: \"{}\"", MappingsInfoCollectionUtils.getInfoFrom(currentMappingsState, false).replace("\n", " "));
         CmdDeleteClient.LOGGER.debug("Mappings registry: {}", currentMappingsState.mappings().getRegistry());
     }
