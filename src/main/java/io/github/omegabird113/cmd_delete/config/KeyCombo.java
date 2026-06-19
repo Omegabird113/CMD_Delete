76758af162c615d3fb88f9e15f1daf25a6cb54cf
omegabird113 <omegabird113@outlook.com>
2026-06-19 09:37:43 -0400

    Make KeyCombo.toString() convert to custom keynames when possible & optimize its constants a bit

--- src/main/java/io/github/omegabird113/cmd_delete/config/KeyCombo.java
+++ src/main/java/io/github/omegabird113/cmd_delete/config/KeyCombo.java
@@ -3,16 +3,23 @@ package io.github.omegabird113.cmd_delete.config;
 import io.github.omegabird113.cmd_delete.mappings.Os;
 import org.jspecify.annotations.NonNull;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public record KeyCombo(int key, boolean shift, boolean altOption, boolean control,
                        boolean superCommand) {
 
+    private static final Map<String, Integer> keyMap = KeyCodeRegistry.getKeyMap();
+    private static final Map<Integer, String> reversedKeyMap = keyMap.entrySet().stream().collect(
+            Collectors.toUnmodifiableMap(Map.Entry::getValue, Map.Entry::getKey));
+    private static final boolean isMac = Os.getCurrent() == Os.MAC;
+
     @Override
     public @NonNull String toString() {
-        boolean isMac = Os.getCurrent() == Os.MAC;
         return (control ? "ctrl+" : "")
                 + (superCommand ? (isMac ? "cmd+" : "sup+") : "")
                 + (altOption ? (isMac ? "opt+" : "alt+") : "")
                 + (shift ? "shift+" : "") +
-                key;
+                (reversedKeyMap.get(key) != null ? reversedKeyMap.get(key) : key);
     }
 }
