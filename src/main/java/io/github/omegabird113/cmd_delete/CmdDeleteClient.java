ca9f5a77655e74f6dcec328eab1099662af931ce
omegabird113 <omegabird113@outlook.com>
2026-06-22 12:16:38 -0400

    Improve log class separators

--- src/main/java/io/github/omegabird113/cmd_delete/CmdDeleteClient.java
+++ src/main/java/io/github/omegabird113/cmd_delete/CmdDeleteClient.java
@@ -35,7 +35,7 @@ public final class CmdDeleteClient implements ClientModInitializer {
     }
 
     public static Logger getLogger(Class<?> clazz) {
-        return LoggerFactory.getLogger(MODID + " " + clazz.getSimpleName());
+        return LoggerFactory.getLogger(MODID + ": " + clazz.getSimpleName());
     }
 
     private static Logger getLogger() {
