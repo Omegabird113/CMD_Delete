7dc1d27fc088a4c5d99a55ac3eeb02c8958c34d9
omegabird113 <omegabird113@outlook.com>
2026-07-03 14:17:00 -0400

    Create new override action names

--- src/main/java/io/github/omegabird113/cmd_delete/actions/ActionOffsetUtils.java
+++ src/main/java/io/github/omegabird113/cmd_delete/actions/ActionOffsetUtils.java
@@ -15,12 +15,12 @@ public final class ActionOffsetUtils {
     @Contract(pure = true)
     public static int getOffset(@NonNull NavAction action) {
         return switch (action) {
-            case NAV_LINE_LEFT, SEL_LINE_LEFT, DEL_LINE_LEFT, NAV_WORD_LEFT, SEL_WORD_LEFT, DEL_WORD_LEFT ->
+            case NAV_LINE_LEFT, SEL_LINE_LEFT, DEL_LINE_LEFT, NAV_WORD_LEFT, SEL_WORD_LEFT, DEL_WORD_LEFT, OVR_NAV_CHAR_LEFT, OVR_DEL_CHAR_LEFT, OVR_SEL_CHAR_LEFT ->
                     OFFSET_LEFT;
-            case NAV_LINE_RIGHT, SEL_LINE_RIGHT, DEL_LINE_RIGHT, NAV_WORD_RIGHT, SEL_WORD_RIGHT, DEL_WORD_RIGHT, NONE ->
+            case NAV_LINE_RIGHT, SEL_LINE_RIGHT, DEL_LINE_RIGHT, NAV_WORD_RIGHT, SEL_WORD_RIGHT, DEL_WORD_RIGHT, OVR_DEL_CHAR_RIGHT, OVR_NAV_CHAR_RIGHT, OVR_SEL_CHAR_RIGHT, NONE ->
                     OFFSET_RIGHT;
-            case NAV_TEXT_START, SEL_TEXT_START, SEL_TEXT_UP -> OFFSET_UP;
-            case NAV_TEXT_END, SEL_TEXT_END, SEL_TEXT_DOWN -> OFFSET_DOWN;
+            case NAV_TEXT_START, SEL_TEXT_START, SEL_TEXT_UP, OVR_NAV_TEXT_UP -> OFFSET_UP;
+            case NAV_TEXT_END, SEL_TEXT_END, SEL_TEXT_DOWN, OVR_NAV_TEXT_DOWN -> OFFSET_DOWN;
         };
     }
 
@@ -29,7 +29,20 @@ public final class ActionOffsetUtils {
         return switch (action) {
             case NAV_LINE_LEFT, NAV_LINE_RIGHT,
                  NAV_WORD_LEFT, NAV_WORD_RIGHT,
-                 NAV_TEXT_START, NAV_TEXT_END -> true;
+                 NAV_TEXT_START, NAV_TEXT_END,
+                 OVR_NAV_CHAR_LEFT, OVR_NAV_CHAR_RIGHT,
+                 OVR_NAV_TEXT_UP, OVR_NAV_TEXT_DOWN -> true;
+            default -> false;
+        };
+    }
+
+    @Contract(pure = true)
+    public static boolean isOverrideAction(@NonNull NavAction action) {
+        return switch (action) {
+            case OVR_NAV_CHAR_LEFT, OVR_DEL_CHAR_LEFT,
+                 OVR_DEL_CHAR_RIGHT, OVR_NAV_CHAR_RIGHT,
+                 OVR_NAV_TEXT_DOWN, OVR_NAV_TEXT_UP,
+                 OVR_SEL_CHAR_LEFT, OVR_SEL_CHAR_RIGHT -> true;
             default -> false;
         };
     }
