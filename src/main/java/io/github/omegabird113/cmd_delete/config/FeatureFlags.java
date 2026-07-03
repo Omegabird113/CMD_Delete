a2bba868e24a1552d34851b27e999237a617d8da
omegabird113 <omegabird113@outlook.com>
2026-07-03 16:41:26 -0400

    Fix wrong variable in MappingsJSONDeserializer.parseFlags()

--- src/main/java/io/github/omegabird113/cmd_delete/config/FeatureFlags.java
+++ src/main/java/io/github/omegabird113/cmd_delete/config/FeatureFlags.java
@@ -3,19 +3,9 @@ package io.github.omegabird113.cmd_delete.config;
 public record FeatureFlags(Boolean overrideVanillaNavigation,
                            Boolean crossLineSignMovement) {
     static FeatureFlags merge(FeatureFlags parent, FeatureFlags child) {
-        boolean overrideVanillaNavigation;
-        boolean crossLineSignMovement;
-
-        if (child.overrideVanillaNavigation == null)
-            overrideVanillaNavigation = parent.overrideVanillaNavigation;
-        else
-            overrideVanillaNavigation = child.overrideVanillaNavigation;
-
-        if (child.crossLineSignMovement == null)
-            crossLineSignMovement = parent.crossLineSignMovement;
-        else
-            crossLineSignMovement = child.crossLineSignMovement;
-
-        return new FeatureFlags(overrideVanillaNavigation, crossLineSignMovement);
+        return new FeatureFlags(
+                child.overrideVanillaNavigation() != null ? child.overrideVanillaNavigation() : parent.overrideVanillaNavigation(),
+                child.crossLineSignMovement() != null ? child.crossLineSignMovement() : parent.crossLineSignMovement()
+        );
     }
 }
