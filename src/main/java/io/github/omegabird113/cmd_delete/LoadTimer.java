4741562a7f9822aafb4a8400925f7454e45d7776
omegabird113 <omegabird113@outlook.com>
2026-07-19 11:30:38 -0400

    Improve load timing code + keep a static FabricLoader

---
+++ src/main/java/io/github/omegabird113/cmd_delete/LoadTimer.java
@@ -0,0 +1,22 @@
+package io.github.omegabird113.cmd_delete;
+
+import org.slf4j.Logger;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoadTimer {
+    private static final Logger LOGGER = LoggingManager.getLogger(LoadTimer.class);
+
+    static void time(Runnable toTime, String name, boolean detailed) {
+        final long startTime = System.nanoTime();
+        toTime.run();
+        final long endTime = System.nanoTime();
+        if (detailed) {
+            final double duration = (endTime - startTime) / 1000000.0f;
+            LOGGER.debug("Timed {}. Took {} ms", name, duration);
+        } else {
+            final long duration = TimeUnit.NANOSECONDS.toMillis(endTime - startTime);
+            LOGGER.info("Timed {}. Took {} ms", name, duration);
+        }
+    }
+}
