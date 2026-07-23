3320cce8bceb1287fd6233db49eb00cfee1fd611
omegabird113 <omegabird113@outlook.com>
2026-07-23 18:18:28 -0400

    Don't use nested enums and simplify code & naming for NavAction

--- src/main/java/io/github/omegabird113/cmd_delete/actions/NavAction.java
+++ src/main/java/io/github/omegabird113/cmd_delete/actions/NavAction.java
@@ -7,39 +7,43 @@ import org.slf4j.Logger;
 
 import java.util.Locale;
 
+import static io.github.omegabird113.cmd_delete.actions.NavActionOffset.*;
+import static io.github.omegabird113.cmd_delete.actions.NavActionScope.*;
+import static io.github.omegabird113.cmd_delete.actions.NavActionType.*;
+
 @SuppressWarnings("unused")
 public enum NavAction {
-    NAV_LINE_LEFT(NavActionOffset.LEFT, Type.MOVE, Scope.WITHIN_LINE, false),
-    NAV_LINE_RIGHT(NavActionOffset.RIGHT, Type.MOVE, Scope.WITHIN_LINE, false),
-    NAV_WORD_LEFT(NavActionOffset.LEFT, Type.MOVE, Scope.WORD, false),
-    NAV_WORD_RIGHT(NavActionOffset.RIGHT, Type.MOVE, Scope.WORD, false),
-    SEL_LINE_LEFT(NavActionOffset.LEFT, Type.SELECT, Scope.WITHIN_LINE, false),
-    SEL_LINE_RIGHT(NavActionOffset.RIGHT, Type.SELECT, Scope.WITHIN_LINE, false),
-    SEL_WORD_LEFT(NavActionOffset.LEFT, Type.SELECT, Scope.WORD, false),
-    SEL_WORD_RIGHT(NavActionOffset.RIGHT, Type.SELECT, Scope.WORD, false),
-    DEL_LINE_LEFT(NavActionOffset.LEFT, Type.DELETE, Scope.WITHIN_LINE, false),
-    DEL_LINE_RIGHT(NavActionOffset.RIGHT, Type.DELETE, Scope.WITHIN_LINE, false),
-    DEL_WORD_LEFT(NavActionOffset.LEFT, Type.DELETE, Scope.WORD, false),
-    DEL_WORD_RIGHT(NavActionOffset.RIGHT, Type.DELETE, Scope.WORD, false),
-    NAV_TEXT_START(NavActionOffset.UP, Type.MOVE, Scope.TEXT, false),
-    NAV_TEXT_END(NavActionOffset.DOWN, Type.MOVE, Scope.TEXT, false),
-    SEL_TEXT_START(NavActionOffset.UP, Type.SELECT, Scope.TEXT, false),
-    SEL_TEXT_END(NavActionOffset.DOWN, Type.SELECT, Scope.TEXT, false),
-    SEL_TEXT_UP(NavActionOffset.UP, Type.SELECT, Scope.LINE, false),
-    SEL_TEXT_DOWN(NavActionOffset.DOWN, Type.SELECT, Scope.LINE, false),
-    OVR_NAV_CHAR_LEFT(NavActionOffset.LEFT, Type.MOVE, Scope.CHAR, true),
-    OVR_NAV_CHAR_RIGHT(NavActionOffset.RIGHT, Type.MOVE, Scope.CHAR, true),
-    OVR_SEL_CHAR_LEFT(NavActionOffset.LEFT, Type.SELECT, Scope.CHAR, true),
-    OVR_SEL_CHAR_RIGHT(NavActionOffset.RIGHT, Type.SELECT, Scope.CHAR, true),
-    OVR_DEL_CHAR_LEFT(NavActionOffset.LEFT, Type.DELETE, Scope.CHAR, true),
-    OVR_DEL_CHAR_RIGHT(NavActionOffset.RIGHT, Type.DELETE, Scope.CHAR, true),
-    OVR_NAV_TEXT_UP(NavActionOffset.UP, Type.MOVE, Scope.LINE, true),
-    OVR_NAV_TEXT_DOWN(NavActionOffset.DOWN, Type.MOVE, Scope.LINE, true),
-    OVR_COPY(NavActionOffset.INVALID, Type.EDIT, Scope.TEXT, true),
-    OVR_CUT(NavActionOffset.INVALID, Type.EDIT, Scope.TEXT, true),
-    OVR_PASTE(NavActionOffset.INVALID, Type.EDIT, Scope.TEXT, true),
-    OVR_SELECT_ALL(NavActionOffset.INVALID, Type.EDIT, Scope.TEXT, true),
-    NONE(NavActionOffset.INVALID, Type.NONE, Scope.NONE, false);
+    NAV_LINE_LEFT(LEFT, MOVE, WITHIN_LINE, false),
+    NAV_LINE_RIGHT(RIGHT, MOVE, WITHIN_LINE, false),
+    NAV_WORD_LEFT(LEFT, MOVE, WORD, false),
+    NAV_WORD_RIGHT(RIGHT, MOVE, WORD, false),
+    SEL_LINE_LEFT(LEFT, SELECT, WITHIN_LINE, false),
+    SEL_LINE_RIGHT(RIGHT, SELECT, WITHIN_LINE, false),
+    SEL_WORD_LEFT(LEFT, SELECT, WORD, false),
+    SEL_WORD_RIGHT(RIGHT, SELECT, WORD, false),
+    DEL_LINE_LEFT(LEFT, DELETE, WITHIN_LINE, false),
+    DEL_LINE_RIGHT(RIGHT, DELETE, WITHIN_LINE, false),
+    DEL_WORD_LEFT(LEFT, DELETE, WORD, false),
+    DEL_WORD_RIGHT(RIGHT, DELETE, WORD, false),
+    NAV_TEXT_START(UP, MOVE, TEXT, false),
+    NAV_TEXT_END(DOWN, MOVE, TEXT, false),
+    SEL_TEXT_START(UP, SELECT, TEXT, false),
+    SEL_TEXT_END(DOWN, SELECT, TEXT, false),
+    SEL_TEXT_UP(UP, SELECT, LINE, false),
+    SEL_TEXT_DOWN(DOWN, SELECT, LINE, false),
+    OVR_NAV_CHAR_LEFT(LEFT, MOVE, CHAR, true),
+    OVR_NAV_CHAR_RIGHT(RIGHT, MOVE, CHAR, true),
+    OVR_SEL_CHAR_LEFT(LEFT, SELECT, CHAR, true),
+    OVR_SEL_CHAR_RIGHT(RIGHT, SELECT, CHAR, true),
+    OVR_DEL_CHAR_LEFT(LEFT, DELETE, CHAR, true),
+    OVR_DEL_CHAR_RIGHT(RIGHT, DELETE, CHAR, true),
+    OVR_NAV_TEXT_UP(UP, MOVE, LINE, true),
+    OVR_NAV_TEXT_DOWN(DOWN, MOVE, LINE, true),
+    OVR_COPY(INVALID, EDIT, TEXT, true),
+    OVR_CUT(INVALID, EDIT, TEXT, true),
+    OVR_PASTE(INVALID, EDIT, TEXT, true),
+    OVR_SELECT_ALL(INVALID, EDIT, TEXT, true),
+    NONE(INVALID, NO_TYPE, NO_SCOPE, false);
 
     private static final Logger LOGGER = LoggingManager.getLogger(NavAction.class);
 
@@ -48,11 +52,11 @@ public enum NavAction {
     }
 
     private final @NonNull NavActionOffset offset;
-    private final @NonNull Type type;
-    private final @NonNull Scope scope;
+    private final @NonNull NavActionType type;
+    private final @NonNull NavActionScope scope;
     private final boolean overrideMode;
 
-    NavAction(@NonNull NavActionOffset offset, @NonNull Type type, @NonNull Scope scope, boolean overrideMode) {
+    NavAction(@NonNull NavActionOffset offset, @NonNull NavActionType type, @NonNull NavActionScope scope, boolean overrideMode) {
         this.offset = offset;
         this.overrideMode = overrideMode;
         this.type = type;
@@ -90,22 +94,22 @@ public enum NavAction {
 
     @Contract(pure = true)
     public boolean isMove() {
-        return this.type == Type.MOVE;
+        return this.type == MOVE;
     }
 
     @Contract(pure = true)
     public boolean isSelect() {
-        return this.type == Type.SELECT;
+        return this.type == SELECT;
     }
 
     @Contract(pure = true)
     public boolean isDelete() {
-        return this.type == Type.DELETE;
+        return this.type == DELETE;
     }
 
     @Contract(pure = true)
     public boolean isEdit() {
-        return this.type == Type.EDIT;
+        return this.type == EDIT;
     }
 
     @Contract(pure = true)
@@ -115,50 +119,42 @@ public enum NavAction {
 
     @Contract(pure = true)
     public boolean isChar() {
-        return this.scope == Scope.CHAR;
+        return this.scope == CHAR;
     }
 
     @Contract(pure = true)
     public boolean isWord() {
-        return this.scope == Scope.WORD;
+        return this.scope == WORD;
     }
 
     @Contract(pure = true)
     public boolean isWithinLine() {
-        return this.scope == Scope.WITHIN_LINE;
+        return this.scope == WITHIN_LINE;
     }
 
     @Contract(pure = true)
     public boolean isLine() {
-        return this.scope == Scope.LINE;
+        return this.scope == LINE;
     }
 
     @Contract(pure = true)
     public boolean isText() {
-        return this.scope == Scope.TEXT;
+        return this.scope == TEXT;
     }
 
     public @NonNull NavActionOffset offset() {
         return offset;
     }
 
-    public @NonNull Type type() {
+    public @NonNull NavActionType type() {
         return type;
     }
 
-    public @NonNull Scope scope() {
+    public @NonNull NavActionScope scope() {
         return scope;
     }
 
     public boolean overrideMode() {
         return overrideMode;
-    }
-
-    public enum Type {
-        MOVE, SELECT, DELETE, EDIT, NONE
-    }
-
-    public enum Scope {
-        NONE, CHAR, WORD, WITHIN_LINE, LINE, TEXT
     }
 }
