677bcd943b31c081f69fe0640b26f16984c02956
omegabird113 <omegabird113@outlook.com>
2026-07-23 10:35:32 -0400

    Avoid HashMap variable in KeyCombo.toString()

--- src/main/java/io/github/omegabird113/cmd_delete/config/data/KeyCombo.java
+++ src/main/java/io/github/omegabird113/cmd_delete/config/data/KeyCombo.java
@@ -3,19 +3,16 @@ package io.github.omegabird113.cmd_delete.config.data;
 import io.github.omegabird113.cmd_delete.mappings.Os;
 import org.jspecify.annotations.NonNull;
 
-import java.util.Map;
-
 public record KeyCombo(int key, boolean shift, boolean altOption, boolean control,
                        boolean superCommand) {
     @Override
     public @NonNull String toString() {
-        final Map<Integer, String> reversedKeyMap = KeyNameRegistry.getReverseKeyMap();
         return "<" +
                 (control ? "ctrl+" : "")
                 + (superCommand ? (Os.IS_USING_MAC ? "cmd+" : "sup+") : "")
                 + (altOption ? (Os.IS_USING_MAC ? "opt+" : "alt+") : "")
                 + (shift ? "shift+" : "")
-                + (reversedKeyMap.getOrDefault(key, Integer.toString(key)))
+                + (KeyNameRegistry.getReverseKeyMap().getOrDefault(key, Integer.toString(key)))
                 + ">";
     }
 }
