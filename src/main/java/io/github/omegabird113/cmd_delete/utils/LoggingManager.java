3c7911357191d0a0b8eb6f42713d4858551621f0
omegabird113 <omegabird113@outlook.com>
2026-07-31 13:36:12 -0400

    Remove accidental JetBrains @NotNull annotations in LoggingManager

--- src/main/java/io/github/omegabird113/cmd_delete/utils/LoggingManager.java
+++ src/main/java/io/github/omegabird113/cmd_delete/utils/LoggingManager.java
@@ -2,7 +2,6 @@ package io.github.omegabird113.cmd_delete.utils;
 
 import io.github.omegabird113.cmd_delete.CmdDeleteClient;
 import org.jetbrains.annotations.Contract;
-import org.jetbrains.annotations.NotNull;
 import org.jspecify.annotations.NonNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,7 +21,7 @@ public final class LoggingManager {
     }
 
     @Contract("_, _ -> new")
-    public static @NonNull Logger getLoggerFor(final @NotNull String modid, final @NonNull Class<?> clazz) {
+    public static @NonNull Logger getLoggerFor(final @NonNull String modid, final @NonNull Class<?> clazz) {
         return LoggerFactory.getLogger(modid + "/" + clazz.getSimpleName());
     }
 
@@ -32,7 +31,7 @@ public final class LoggingManager {
     }
 
     @Contract("_, _ -> new")
-    public static @NonNull Logger getLoggerFor(final @NotNull String modid, final @NonNull Object o) {
+    public static @NonNull Logger getLoggerFor(final @NonNull String modid, final @NonNull Object o) {
         return LoggerFactory.getLogger(modid + "/" + o.getClass().getSimpleName() + "@" + Integer.toHexString(System.identityHashCode(o)));
     }
 
