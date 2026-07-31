53f9da194950ff3dad4ca587282be678731b8187
omegabird113 <omegabird113@outlook.com>
2026-07-30 21:03:31 -0400

    Add API method for getting loggers for other mod IDs

--- src/main/java/io/github/omegabird113/cmd_delete/utils/LoggingManager.java
+++ src/main/java/io/github/omegabird113/cmd_delete/utils/LoggingManager.java
@@ -2,6 +2,7 @@ package io.github.omegabird113.cmd_delete.utils;
 
 import io.github.omegabird113.cmd_delete.CmdDeleteClient;
 import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 import org.jspecify.annotations.NonNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,8 +11,13 @@ public final class LoggingManager {
     private LoggingManager() {
     }
 
+    @Contract("_, _ -> new")
+    public static @NonNull Logger getLogger(@NotNull String modid, @NonNull Class<?> clazz) {
+        return LoggerFactory.getLogger(modid + "/" + clazz.getSimpleName());
+    }
+
     @Contract("_ -> new")
     public static @NonNull Logger getLogger(@NonNull Class<?> clazz) {
-        return LoggerFactory.getLogger(CmdDeleteClient.MODID + "/" + clazz.getSimpleName());
+        return getLogger(CmdDeleteClient.MODID, clazz);
     }
 }
